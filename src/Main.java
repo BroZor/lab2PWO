f8f96223b36e4463750c4ba293100875858fcbd7
BroZor <kubulba1988@gmail.com>
2024-10-17 15:52:17 +0200

    third commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world! 40k");
         System.out.println("Hello world! 30k");
+        System.out.println("Hello world! 301k");
     }
 }
